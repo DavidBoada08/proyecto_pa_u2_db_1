97eb3efd0035fbe36bbbe7eee43189af06ed3e1d
DavidBoada08 <dfboadaa@uce.edu.ec>
2023-01-23 22:31:32 -0500

    taller21 correcion

---
+++ src/main/java/com/example/demo/modelo/Habitacion.java
@@ -0,0 +1,60 @@
+package com.example.demo.modelo;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "habitacion")
+public class Habitacion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "habi_seq")
+	@SequenceGenerator(name = "habi_seq", sequenceName = "habi_seq", allocationSize = 1)
+	@Column(name = "habi_id")
+	private Integer id;
+	@Column(name = "habi_numero")
+	private String numero;
+
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "habi_id_hotel")
+	private Hotel hotel;
+
+	// GETTER & SETTER
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	// TO STRING
+	@Override
+	public String toString() {
+		return "Habitacion [id=" + id + ", numero=" + numero + "]";
+	}	
+}
